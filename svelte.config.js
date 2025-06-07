1214d83ae2694e42f7f1a69a536db7139e45bcac
María Guillermina Véscovo <guilleves@gmail.com>
2025-06-07 16:01:21 -0300

    gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,25 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static'; // <-- Import the adapter
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-}
+
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter({
+      // default options are fine for most cases
+      pages: 'build',
+      assets: 'build',
+      fallback: undefined, // or '404.html' or 'index.html'
+      precompress: false,
+      strict: true
+    })
+  }
+};
+
+export default config;
