abc9bc5e8af6d261807a2ee6a3b431f981e59c79
María Guillermina Véscovo <guilleves@gmail.com>
2025-06-08 00:12:22 -0300

    redirect to /

--- svelte.config.js
+++ svelte.config.js
@@ -19,9 +19,6 @@ const config = {
       precompress: false,
       strict: true
     })
-    // paths: {
-    //   base: process.env.NODE_ENV === 'production' ? '/pairforge-spa' : '',
-    // }
   }
 };
 
